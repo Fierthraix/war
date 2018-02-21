c7316f202ce35dd2b708eb6e7fdd4eb12fd09daf
Lucas Rooyakkers <lucas.rooyakkers@protonmail.com>
2018-02-21 16:25:43 -0500

    Added main to perform the calculation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+extern crate rayon;
+extern crate war;
+
+use war::WarGame;
+use rayon::prelude::*;
+
+const NUM_ROUNDS: usize = 1000;
+const MAX_TURNS: usize = 10_000_000;
+
 fn main() {
-    println!("Hello, world!");
+    let totals = (0..NUM_ROUNDS).into_par_iter().filter_map(|_|{
+        let mut game = WarGame::new();
+        let mut turns = 0usize;
+
+        while !game.game_over {
+            if turns >= MAX_TURNS {
+                return None;
+            }
+            game.turn();
+            turns += 1;
+        }
+        Some(turns)
+    }).collect::<Vec<_>>();
+    let average_turns = totals.iter().map(|&n| n).sum::<usize>() / totals.len();
+    println!("Average num of turns: {}", average_turns);
 }
